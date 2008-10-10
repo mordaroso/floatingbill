e097060302c0ad1aca1d494c09279101f6b7763e
mordaroso <admin@mordaroso.net>
2008-10-10 09:39:53 +0200

    fix mail bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,15 @@ config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
-# Don't care if the mailer can't send
+
 config.action_mailer.default_charset = "utf-8"
+
+# Tell Action Mailer not to deliver emails to the real world.
+# The :test delivery method accumulates sent emails in the
+# ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
-#config.action_mailer.raise_delivery_errors = false
+
+# Don't care if the mailer can't send
+# config.action_mailer.raise_delivery_errors = false
+
 config.active_record.observers = :user_observer, :bill_observer, :transfer_observer
