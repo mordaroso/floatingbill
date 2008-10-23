da57e83ed740e248b7a9cd4d1f39ad35aa02c39a
mordaroso <admin@mordaroso.net>
2008-10-23 21:56:02 +0200

    finish rss feed

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,21 +1,41 @@
 class News
 
-  attr_accessor :type, :id, :text, :time
+  attr_accessor :type, :object, :verb, :time, :user
 
-  def initialize(type, id, text, time)
+  def initialize(type, object, verb, time, user)
     self.type = type
-    self.id = id
-    self.text = text
+    self.object = object
+    self.verb = verb
     self.time = time
+    self.user = user
   end
 
 
   def self.get_all_by_user(user)
     news = Array.new
-    for payment in user.payments
-      news << News.new('Bill', payment.bill.id, 'Bill created', payment.bill.created_at)
+
+    #bills and payments
+    for bill in Bill.by_user_id(user.id)
+      #created
+      news << News.new('bill', bill, 'created', bill.created_at, bill.creator) unless bill.creator == user
+
+      # accepted
+      for payment in bill.payments.closed
+        news << News.new('bill', bill, 'accepted', payment.accepted_at, payment.payer) unless payment.payer == user
+      end
+
+      # closed
+      news << News.new('bill', bill, 'closed', bill.created_at, bill.creator) unless bill.closed_at.blank? or bill.creator == user
     end
+
+    #transfer
+    for transfer in Transfer.find_all_by_user_id(user.id)
+      news << News.new('transfer', transfer, 'created', transfer.created_at, transfer.debitor) unless bill.creator == user
+      news << News.new('transfer', transfer, 'verified', transfer.verified_at, transfer.creditor) unless !transfer.verified? || bill.creator == user
+    end
+
     news.sort_by {|n| n.time}
   end
 
 end
+
--- app/views/users/show.rss.builder
+++ app/views/users/show.rss.builder
@@ -5,13 +5,20 @@ xml.rss :version => "2.0" do
     xml.description meta_description
     xml.link rss_url (@user)
 
-    for payment in @user.payments
+    for news in News.get_all_by_user(@user)
       xml.item do
-        xml.title payment.bill.name
-        xml.description payment.bill.description
-        xml.pubDate payment.bill.created_at.to_s(:rfc822)
-        xml.link bill_url(payment.bill)
-        xml.guid bill_url(payment.bill)
+        xml.title "#{news.user.login} #{news.verb} #{news.type}"
+        case news.type
+          when 'bill'
+            xml.description news.object.description
+            xml.link bill_url(news.object)
+            xml.guid bill_url(news.object)
+          when 'transfer'
+            xml.description 'Transfer'
+            xml.link transfer_url(news.object)
+            xml.guid transfer_url(news.object)
+          end
+        xml.pubDate news.time.to_s(:rfc822)
       end
     end
 
