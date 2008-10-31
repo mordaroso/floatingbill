b0d7f7556b3c760c9ba7c0a8a491a660d6536436
mordaroso <admin@mordaroso.net>
2008-10-31 22:34:51 +0100

    change Capistrano tasks

--- app/models/bill_observer.rb
+++
@@ -1,8 +0,0 @@
-class BillObserver < ActiveRecord::Observer
-  def after_create(bill)
-    for payment in bill.payments
-      UserMailer.deliver_payment_notification(payment, bill) unless payment.payer == bill.creator
-    end
-  end
-
-end
--- lib/cap_recipies.rb
+++ lib/cap_recipies.rb
@@ -1,19 +1,10 @@
 Capistrano::Configuration.instance(:must_exist).load do
 
-  #############################################################
-  #	Database.yml
-  #############################################################
-
-  desc "Link to the real database.yml"
-  task :link_database, :role => :app do
-    run "link /srv/web/floatingbill/database.yml /srv/web/floatingbill/current/config/database.yml"
-  end
-
-  #############################################################
-  #	Passenger
-  #############################################################
-
   namespace :deploy do
+    #############################################################
+    #	Passenger
+    #############################################################
+
     desc "Restarting mod_rails with restart.txt"
     task :restart, :roles => :app, :except => { :no_release => true } do
       run "touch #{current_path}/tmp/restart.txt"
@@ -23,7 +14,36 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc "#{t} task is a no-op with mod_rails"
       task t, :roles => :app do ; end
     end
+
+    #############################################################
+    #	Database.yml
+    #############################################################
+
+    desc "Link to the real database.yml"
+    task :link_database, :role => :app do
+      run "link #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    end
   end
+
+  #############################################################
+  #	BackgrounDRb
+  #############################################################
+  namespace :brb do
+
+    desc "start BackgrounDRb"
+    task :start, :role => :app do
+      run "#{current_path}/script/backgroundrb start"
+    end
+
+    desc "stop BackgrounDRb"
+    task :stop, :role => :app do
+      run "#{current_path}/script/backgroundrb stop"
+    end
+  end
+
+after 'deploy:symlink', 'deploy:link_database'
+before 'deploy:restart', 'brb:stop'
+after 'deploy:restart', 'brb:start'
 
 end
 
